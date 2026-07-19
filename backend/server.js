1fcaebb89a63e69a08c00bc0cd2d09575b302bc1
Muhammad Adnan <workforyou.dev@gmail.com>
2026-07-19 03:10:45 -0700

    for vercel deploy

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,8 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// Vercel ke liye export
+module.exports = app;
